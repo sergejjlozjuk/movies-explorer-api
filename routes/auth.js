f52dd3c662cdbe3ea036211b16a96bfbe3dfa5cf
sergejjlozjuk <50342457+sergejjlozjuk@users.noreply.github.com>
2023-02-28 15:25:57 +0300

    change validation in signup router

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ const { createUser, login } = require('../controllers/users');
 authRouter.post('/signup', celebrate({
   body: {
     email: Joi.string().email().required(),
-    name: Joi.string().required(),
+    name: Joi.string().required().min(2).max(30),
     password: Joi.string().required(),
   },
 }), createUser);
